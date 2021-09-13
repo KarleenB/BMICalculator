cf894e63c9a19782ca5a2bb28e1e556aed28af49
Branden Karleen <karleen@MacBook-Karleen.local>
2021-09-13 15:48:45 -0700

    Initial Commit

---
+++ BMI Calculator/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Branden Karleen on 5/7/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
